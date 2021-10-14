e4d8b98100943a2e00d9abbdf305669a0e6d05dc
Ronald Alves <ronald.alves7@outlook.com>
2021-10-14 17:40:23 -0300

    third commit

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
 const menu = document.querySelector('#menu-bnt');
 const navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
-document.querySelector('.login-btn').onclick = () =>{
+document.querySelector('.login-btn').onclick = () => {
     document.querySelector('.login-form-container').classList.toggle('active');
 
 }
 
-document.querySelector('#close-login-form').onclick = () =>{
+document.querySelector('#close-login-form').onclick = () => {
     document.querySelector('.login-form-container').classList.remove('active');
 
 }
 
-
-window.onscroll = () =>{
-    if(window.scrollY > 0){
+window.onscroll = () => {
+    if (window.scrollY > 0) {
         document.querySelector('.header').classList.add('active')
-    }else{
+    } else {
         document.querySelector('.header').classList.remove('active')
     }
     menu.classList.remove('fa-times');
@@ -28,23 +27,59 @@ window.onscroll = () =>{
 }
 
 window.onload = () => {
-    if(window.scrollY > 0){
+    if (window.scrollY > 0) {
         document.querySelector('.header').classList.add('active')
-    }else{
+    } else {
         document.querySelector('.header').classList.remove('active')
     }
 }
 
-document.querySelector('.home').onmousemove = (e) =>{
+document.querySelector('.home').onmousemove = (e) => {
+    document.querySelectorAll('.home-parallax').forEach(elm => {
 
-    document.querySelectorAll('.home-parallax').forEach(elm =>{
+        const speed = elm.getAttribute('data-speed');
 
-        let speed = elm.getAttribute('data-speed');
+        const x = (window.innerWidth - e.pageX * speed) / 90;
+        const y = (window.innerHeight - e.pageY * speed) / 90;
+        elm.style.transform = `translateX(${y}px) translateY(${x}px)`
+    });
+};
 
-        let x = (window.innerWidth - e.pageX * speed) / 90;
-        let y = (window.innerHeight - e.pageY * speed) / 90;
+document.querySelector('.home').onmouseleave = () => {
+    document.querySelectorAll('.home-parallax').forEach(elm => {
 
-        elm.style.transform = `translateX(${y})px translateY(${x})px`
-    })
-}
+        elm.style.transform = `translateX(0px) translateY(0px)`
+    });
+};
+
+
+var swiper = new Swiper(".vehicles-sliders", {
+    grabCursor: true,
+    centeredSlides: true,  
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+
+
 
